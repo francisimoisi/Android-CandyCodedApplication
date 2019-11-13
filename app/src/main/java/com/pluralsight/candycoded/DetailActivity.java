dca2dd23e0d1506f55a97eeb4e1e0372f7648b00
francis Imoisi <francisimoisi@yahoo.com>
2019-11-13 08:18:11 +0100

    Update DetailActivity.java

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -81,7 +81,8 @@ public class  DetailActivity extends AppCompatActivity {
     private void createShareIntent() {
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_TEXT, SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED);
+        String sharedString = SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED;
+        shareIntent.putExtra(Intent.EXTRA_TEXT, sharedString);
 
         startActivity(shareIntent);
     }
